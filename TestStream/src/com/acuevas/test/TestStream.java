9f3be541d0930146eebd30e7366ae67216937d6f
Alex <alexcuevitas@gmail.com>
2018-12-14 22:46:09 +0100

    Finished propagating the exception through the Stream.forEach.

--- TestStream/src/com/acuevas/test/TestStream.java
+++ TestStream/src/com/acuevas/test/TestStream.java
@@ -5,15 +5,40 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-public class TestStream {
+public class TestStream<T> {
 
-	private void readAllTest() {
-		List<String> strings = Arrays.asList("aa", "bb");
+	private static List<String> strings = Arrays.asList("aa", "bb");
 
-		strings.
+	private static void readAll() {
+		try {
+			strings.stream().map(File::new).forEach(TestStream::safeRead);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void read(File file) throws IOException {
+	private static void safeRead(File file) throws RuntimeException {
+		// wraps the exception into a RuntimeException to be unchecked and get through
+		// the forEach
+		try {
+			read(file);
+		} catch (IOException e) {
+			// I have to instantiate a RuntimeException to set the StackTrace properly.
+			RuntimeException runtimeException = new RuntimeException(e);
+			runtimeException.setStackTrace(e.getStackTrace());
+			throw runtimeException;
+		}
+	}
 
+	private static void read(File file) throws IOException {
+		// just doing random things to test the exception
+		if (file.getName() == "aa")
+			System.out.println(file.getName());
+		if (file.getName() == "bb")
+			throw new IOException("ERROR");
+	}
+
+	public static void main(String[] args) {
+		readAll();
 	}
 }
